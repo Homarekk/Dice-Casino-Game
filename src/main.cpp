74bdbe4d3bcc30e7977eaf94df997a909a9efdb8
Homarekk <danielsojka2006@gmail.com>
2025-10-19 21:59:28 +0200

    Implement auto play feature and exit option
    Added auto play feature and exit option to game loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "../include/AccountManager.h"
 #include "../include/Utils.h"
 #include "../include/Game.h"
+#include <conio.h>  // for _kbhit() and _getch()
+
 
 /*
 The Player class contains a std::vector<Transaction> and uses Transaction functions, so we don't have to include it
@@ -31,8 +35,8 @@ int main() {
         // Game loop for logged in user
         while (true) {
             std::cout << "\nWelcome " << player->getName() << "! Balance: $" << player->getBalance() << "\n";
-            std::cout << "1. Play\n2. Deposit\n3. Withdraw\n4. History\n5. Logout\n";
-            int option = Utils::getValidInt("Choose option: ", 1, 5);
+            std::cout << "1. Play\n2. Deposit\n3. Withdraw\n4. History\n5. Auto Play\n6. Exit\n";
+            int option = Utils::getValidInt("Choose option: ", 1, 6);
 
             if (option == 1) {
                 if (player->getBalance() < 1) {
@@ -60,6 +64,39 @@ int main() {
                 player->showHistory();
             }
             else if (option == 5) {
+                if (player->getBalance() < 1) {
+                    std::cout << "Too low balance to place bet\n";
+                    continue;
+                }
+
+                double bet = Utils::getValidDouble("Enter your bet for auto mode: ");
+                if (bet < 1) {
+                    std::cout << "Minimum bet amount is $1\n";
+                    continue;
+                }
+
+                int winRange = Utils::getValidInt("Enter winning range (1-99): ", 1, 99);
+
+                std::cout << "\nAuto mode started. Press ANY key to stop\n";
+                while (true) {
+                    if (_kbhit()) { // user pressed a key
+                        std::cout << "Auto mode stopped\n";
+                        break;
+                    }
+
+                    if (player->getBalance() < bet) {
+                        std::cout << "Not enough balance\n";
+                        break;
+                    }
+
+                    game.playRound(*player, bet, winRange);
+                    std::cout << "Current Balance: $" << player->getBalance() << "\n\n";
+                    manager.saveToFile();
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                }
+            }
+            else if (option == 6) {
                 manager.logout();
                 break;
             }
